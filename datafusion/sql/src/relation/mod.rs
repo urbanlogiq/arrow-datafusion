5f3cb5699bb4e733935bdecb6e77a23ee422ccfc
Max Burke <max@urbanlogiq.com>
2025-04-28 16:15:33 -0700

    regarding https://github.com/apache/datafusion/issues/15886, comment out problematic query transformation

--- datafusion/sql/src/relation/mod.rs
+++ datafusion/sql/src/relation/mod.rs
@@ -150,11 +150,10 @@ impl<'a, S: ContextProvider> SqlToRel<'a, S> {
             }
         };
 
-        let optimized_plan = optimize_subquery_sort(plan)?.data;
         if let Some(alias) = alias {
-            self.apply_table_alias(optimized_plan, alias)
+            self.apply_table_alias(plan, alias)
         } else {
-            Ok(optimized_plan)
+            Ok(plan)
         }
     }
 
