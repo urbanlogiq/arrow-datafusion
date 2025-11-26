e338a74e1b8aec1ad355e8ab3e22dcafc56ec7a9
Max Burke <max@urbanlogiq.com>
2025-11-26 10:34:02 -0800

    Allow coercion from int64 to timestamps

--- datafusion/expr-common/src/type_coercion/binary.rs
+++ datafusion/expr-common/src/type_coercion/binary.rs
@@ -1889,6 +1889,8 @@ fn temporal_coercion_nonstrict_timezone(
 
             Some(Timestamp(unit, tz))
         }
+        (Timestamp(_, _), Int64) => Some(lhs_type.clone()),
+        (Int64, Timestamp(_, _)) => Some(rhs_type.clone()),
         _ => temporal_coercion(lhs_type, rhs_type),
     }
 }
@@ -1936,6 +1938,8 @@ fn temporal_coercion_strict_timezone(
 
             Some(Timestamp(unit, tz))
         }
+        (Timestamp(_, _), Int64) => Some(lhs_type.clone()),
+        (Int64, Timestamp(_, _)) => Some(rhs_type.clone()),
         _ => temporal_coercion(lhs_type, rhs_type),
     }
 }
